b484e1b6a008086bf39dba45bfc1281283f31c38
Brad Woods <brad@easyagile.com>
2019-09-29 20:15:04 +1000

    Add stuled-components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
 
 export default ({ children }) => (
-    <div
-        style={{
-            width: '100%',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
+    <Wrapper>
         {children}
-    </div>
+    </Wrapper>
 )
