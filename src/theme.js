aa0dbfb1f21098fbbac9faf309547b6b1de15c21
Brad Woods <brad@easyagile.com>
2019-09-30 20:31:50 +1000

    Replace mdx theme with styled-components theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export default {
+    color: "pink",
+    fontSize: [
+        "11.11px",
+        "12.8px",
+        "16px",
+        "20px",
+        "25px",
+        "31.25px",
+        "39.06px",
+        "48.83px",
+        "61.04px",
+    ],
+}
+
