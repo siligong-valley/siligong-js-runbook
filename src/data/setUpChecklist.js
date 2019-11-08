44b8a74307c45a5fdd3e1942ba3c5f824ea24c94
Brad Woods <brad@easyagile.com>
2019-11-08 14:01:49 +1100

    Add setUpChecklist data

---
+++ src/data/setUpChecklist.js
@@ -0,0 +1,5 @@
+export default [
+    `order food & drinks (previous meetup - ordered 8 pizzas, 3 salads, should have ordered double)`,
+    `setup seating`,
+    `setup camera (for recording presenters)`
+]
