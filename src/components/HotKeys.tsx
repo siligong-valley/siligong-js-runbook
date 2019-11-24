9455dc918c95a66467cbf7e8a98cf99997003fdf
Brad Woods <brad@easyagile.com>
2019-11-24 16:23:06 +1100

    Add hotkey

---
+++ src/components/HotKeys.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { styled, gridSquares } from "../theme";
+import { GridGutterColumns } from './Grid';
+import Icon from './Icon'
+import { IconKeyType, IconSizeType } from '../types';
+import Deemph from './Deemph';
+
+const Wrapper = styled.p(props => `
+    position: absolute;
+    bottom: ${gridSquares(1)};
+    right: ${gridSquares(1)};
+
+    display: flex;
+    align-items: center;
+`)
+
+const HotKeys: FC = () => (
+    <Wrapper>
+        <Icon iconKey={IconKeyType.keyboard} size={IconSizeType.l} />
+        <GridGutterColumns />
+        <Deemph>
+            'f' for full-screen
+        </Deemph>
+    </Wrapper>
+)
+
+export default HotKeys
